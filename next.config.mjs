64b1b87211c5a53a572951094eb2c43750e57d0a
Jesse Koldewijn <jesse@iungo.nl>
2023-04-24 14:27:26 +0200

    fix: added csp header to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -9,6 +10,8 @@ import NextPWA from "next-pwa";
 // @ts-ignore
 import runtimeCaching from "next-pwa/cache.js";
 
+const remoteURL = "https://jkinsight-next.vercel.app";
+
 const withPWA = NextPWA({
   disable: process.env.NODE_ENV === "development",
   dest: "public",
@@ -19,6 +22,27 @@ const withPWA = NextPWA({
 
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        // Sets security headers for all routes
+        source: "/(.*)",
+        headers: [
+          process.env.NODE_ENV == "development"
+            ? {
+                key: "Content-Security-Policy",
+                value:
+                  "default-src 'self' https://localhost:3000'; style-src 'self' ; image-src 'https://localhost:3000';  script-src 'self' https://localhost:3000; font-src 'self' 'https://localhost:3000'",
+              }
+            : {
+                key: "Content-Security-Policy",
+                value: `default-src 'self' ${remoteURL}'; style-src 'self' ; image-src '${remoteURL}';  script-src 'self' ${remoteURL}; font-src 'self' '${remoteURL}'`,
+              },
+        ],
+      },
+    ];
+  },
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
