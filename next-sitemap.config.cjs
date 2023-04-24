fa60f615f71195eb2595655f051bc93b96ad2918
Jesse Koldewijn <jesse@iungo.nl>
2023-04-24 14:29:41 +0200

    fix: edit of sitemap url pointer

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://jkinsight.nl",
+  siteUrl: process.env.SITE_URL || "https://jkinsight-next.vercel.app",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
