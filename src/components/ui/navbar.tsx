1c9b1c1c5a08b6aa75116e4413baf5beade8474b
Jesse Koldewijn <jesse@iungo.nl>
2023-04-26 14:47:00 +0200

    fix: repair of link component ref in navbar (invalid className call)

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         <>
           <Link href={Href} legacyBehavior passHref>
             <NavigationMenuLink
-              className={navigationMenuTriggerStyle()}
+              className={"my-auto " + navigationMenuTriggerStyle()}
             >
               {Title}
             </NavigationMenuLink>
@@ -49,12 +49,7 @@ const Navbar = () => {
   return (
     <div className="fixed inset-x-0 top-0 flex w-full bg-[rgba(200,200,200,0.75)] px-4 py-3 dark:bg-[rgba(0,0,0,0.75)]">
       <div className="navbar-start flex text-xl">
-        <LinkComponent
-          Href="/"
-          Title="JKinsight"
-          variantPlain
-          className="my-auto"
-        />
+        <LinkComponent Href="/" Title="JKinsight" variantPlain />
       </div>
       <div className="navbar-center flex gap-2">
         <NavigationMenu>
