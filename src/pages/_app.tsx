dbc23155ca770fb512691d982d7ff269f3ff617c
JesseKoldewijn <dev@glitchtech.eu>
2023-04-27 14:37:28 +0200

    fix: small tweak on font usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,12 @@
 import "@/styles/globals.css";
 
 import { useEffect } from "react";
-
-import localFont from "next/font/local";
 import { useRouter } from "next/router";
 
 import { AnimatePresence } from "framer-motion";
 
 import { api } from "@/utils/api";
 import Layout from "@/components/layout";
-
-const JetbrainsMono = localFont({
-  src: "../fonts/JetBrains_Mono_Regular_Nerd_Font_Complete_Mono.ttf",
-});
 
 const RootApp = ({
   Component,
@@ -23,20 +17,6 @@ const RootApp = ({
   pageProps: any;
 }) => {
   const router = useRouter();
-
-  useEffect(() => {
-    const headElem = document.getElementsByTagName("head")[0];
-    if (headElem) {
-      const styleToAdd = `<style>:root { --font-jb-mono: ${JetbrainsMono.style.fontFamily} }</style>`;
-      const lastAdded = Array.from(headElem.children)[
-        headElem.children.length - 1
-      ];
-
-      if (lastAdded?.innerHTML.toString().includes("Jetbrains") == false) {
-        headElem.insertAdjacentHTML("beforeend", styleToAdd);
-      }
-    }
-  }, []);
 
   return (
     <>
